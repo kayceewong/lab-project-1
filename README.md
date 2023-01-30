9988f8ecdd7e61c01e96acc807489da5b8131ab3
kayceewong <kayceewonghk@gmail.com>
2023-01-31 01:02:35 +0800

    Update with categories

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # lab-kc-project-1
+
 Project 1 - Create a Game
+
+Press "New Game" to start a new game
+Pick a category
+Guess the word using letters as input
+For each wrong guess, a line will be drawn for the hangman
+The wrong letter will be blanked out
+Pop up a window to show if you win or lose at the end.
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 //
-const optionsContainer = document.getElementById('options-container');
+const categoryContainer = document.getElementById('options-container');
 const letterContainer = document.getElementById('letter-container');
 const userInputSection = document.getElementById('user-input-section');
 const newGameContainer = document.getElementById('new-game-container');
 const newGameButton = document.getElementById('new-game-button');
 const result = document.getElementById('result-text');
 const canvas = document.getElementById('canvas');
+
+//Options for buttons and related words for the options
+let category = {
+  countries: ["Hong Kong", "Hungary", "Iceland", "Israel", "Albania", "Andorra", "Australia", "Canada", "Venezuela", "India",],
+
+  animals: ["Chicken", "Dolphin", "Elephant", "Hedgehog", "Parrot", "Eagle", "Goldfish", "Squirrel", "Panther", "Swordfish",],
+
+  fruits: ["apple", "banana", "watermelon", "orange", "pineapple", "papaya", "raspberry", "cherry", "lemon", "strawberry"],
+};
+
+//Start New Game
+newGameButton.addEventListener('click', function () {
+  init('reset');
+});
