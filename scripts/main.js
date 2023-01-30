178c6f6b9a98b28a154b98280219e4dd33f8fa7a
kayceewong <kayceewonghk@gmail.com>
2023-01-30 12:29:53 +0800

    Updated files

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//
+const optionsContainer = document.getElementById('options-container');
+const letterContainer = document.getElementById('letter-container');
+const userInputSection = document.getElementById('user-input-section');
+const newGameContainer = document.getElementById('new-game-container');
+const newGameButton = document.getElementById('new-game-button');
+const resultText = document.getElementById('result-text');
+const canvas = document.getElementById('canvas');
