d7ebb8c87e1db651339192d8c7a8eacd9e58310b
kayceewong <kayceewonghk@gmail.com>
2023-01-30 17:02:19 +0800

    Update small changes

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,5 @@ const letterContainer = document.getElementById('letter-container');
 const userInputSection = document.getElementById('user-input-section');
 const newGameContainer = document.getElementById('new-game-container');
 const newGameButton = document.getElementById('new-game-button');
-const resultText = document.getElementById('result-text');
+const result = document.getElementById('result-text');
 const canvas = document.getElementById('canvas');
